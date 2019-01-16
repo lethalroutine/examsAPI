413c6c673f19e2340d286f30b1776d25a0eeee60
aleksander <aleksander.gajda1@gmail.com>
2019-01-16 22:18:03 +0100

    Adding task assessment

--- exams/urls.py
+++ exams/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('exams/', views.ExamList.as_view()),
     path('exams/<int:pk>/', views.ExamDetail.as_view()),
     path('exams/files/<int:pk>/', views.ExamFileDetail.as_view()),
+    path('exams/create', views.ExamCreate.as_view()),
+    path('exams/tasks/<int:pk>', views.TaskDetail.as_view()),
 ]
--- exams/views.py
+++ exams/views.py
@@ -1,16 +1,19 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from rest_framework import status
 from rest_framework.parsers import FileUploadParser
-from exams.models import Exam, ExamFile
-from exams.serializers import ExamSerializer, ExamFileSerializer
+from exams.models import Exam, ExamFile, Task
+from exams.serializers import ExamSerializer,TaskSerializer
 from wsgiref.util import FileWrapper
 import os
 
 
 class ExamList(APIView):
+    """
+    View to list all sent exams
+    """
     def get(self, request, format=None):
         exams = Exam.objects.all()
         serializer = ExamSerializer(exams, many=True)
@@ -18,6 +21,9 @@ class ExamList(APIView):
 
 
 class ExamDetail(APIView):
+    """
+    View to get the details of sent exam with possibility to update
+    """
     def get_object(self, pk):
         try:
             return Exam.objects.get(pk=pk)
@@ -38,7 +44,23 @@ class ExamDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ExamCreate(APIView):
+    """
+    View to create exam
+    """
+    def post(self, request):
+        serializer = ExamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 class ExamFileDetail(APIView):
+    """
+    View to manage exam sheets
+    """
     parser_classes = (FileUploadParser,)
 
     def get(self, request, pk):
@@ -69,3 +91,36 @@ class ExamFileDetail(APIView):
             print('could not delete file ' + "data/" + exam_file.file.name)
         exam_file.file.delete(save=True)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TaskDetail(APIView):
+    """
+    View to assign points to exam task
+    """
+    def get_object(self, pk):
+        try:
+            return Exam.objects.get(pk=pk)
+        except Exam.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        if len(Task.objects.filter(pk=pk)) == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        task = Task.objects.get(pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def post(self, request, pk):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        task = self.get_object(pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
